6b82cb9ea5759dd950cff3fa1b907442cf6e8b01
Wael Elkadim <elkadeem@hotmail.com>
2025-11-11 12:10:11 +0200

    Add web application

---
+++ Webapp/WebApplicationAPI/Model/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplicationAPI.Entities;
+
+namespace WebApplicationAPI.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public AppDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // use local db by default
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=UnitTestSample01Db;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
