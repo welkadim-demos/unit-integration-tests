808cea8a4c6f401b0a50bc6a740ec7ad95bc6daf
Wael Elkadim <elkadeem@hotmail.com>
2025-11-11 13:59:05 +0200

    Add integration tests for WebApplicationAPI
    Introduced a new project, `WebApplicationAPI.IntegrationTests`, to the solution for integration testing. Added a test class, `DepartmentsServiceTests`, to verify the `/api/departments` endpoint. Configured a custom `WebApplicationAPIFactory` to use an in-memory database for isolated and repeatable tests.
    
    Updated the solution file (`UnitTestSamples.sln`) to include the new project and its build configurations. Added necessary dependencies for integration testing, mocking, and database operations.

---
+++ Webapp/WebApplicationAPI.IntegrationTests/DepartmentsServiceTests.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplicationAPI.IntegrationTests
+{
+    public class DepartmentsServiceTests : IClassFixture<WebApplicationAPIFactory>
+    {
+        private readonly HttpClient _client;
+
+        public DepartmentsServiceTests(WebApplicationAPIFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task Get_All_Departments_Return_Success()
+        {
+            // Arrange
+
+            // Act
+            var response = await _client.GetAsync("/api/departments");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+        }
+    }
+}
---
+++ Webapp/WebApplicationAPI.IntegrationTests/WebApplicationAPIFactory.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplicationAPI.Model;
+
+namespace WebApplicationAPI.IntegrationTests
+{
+    public class WebApplicationAPIFactory : WebApplicationFactory<Program>
+    {
+        public WebApplicationAPIFactory()
+        {
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var dbContextDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(IDbContextOptionsConfiguration<AppDbContext>));
+
+                services.Remove(dbContextDescriptor);
+
+                var dbConnectionDescriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbConnection));
+
+                services.Remove(dbConnectionDescriptor);
+
+                // Create open SqliteConnection so EF won't automatically close it.
+                //services.AddSingleton<DbConnection>(container =>
+                //{
+                //    var connection = new SqliteConnection("DataSource=:memory:");
+                //    connection.Open();
+
+                //    return connection;
+                //});
+
+                services.AddDbContext<AppDbContext>((container, options) =>
+                {
+                    // use in memory database 
+                    options.UseInMemoryDatabase("webapitest");
+
+                    //var connection = container.GetRequiredService<DbConnection>();
+                    //options.UseSqlite(connection);
+                });
+            });
+
+            builder.UseEnvironment("Development");
+        }
+    }
+}
+
